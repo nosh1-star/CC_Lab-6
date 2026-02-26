6cf7945f3c850109f31d28c52f47fd674b5453bd
nosh1-star <noshitareddy1234@gmail.com>
2026-02-26 21:58:27 +0530

    Fix HTTP response body construction

--- backend/app.cpp
+++ backend/app.cpp
@@ -41,13 +41,14 @@ int main() {
     while(true) {
         int client_fd = accept(server_fd, NULL, NULL);
         if (client_fd < 0) continue;
-        
+        std::string body = "Served by backend: " + std::string(hostname) + "\n";
         // Simple HTTP response
         std::string response = "HTTP/1.1 200 OK\r\n";
         response += "Content-Type: text/plain\r\n";
         response += "Content-Length: " + std::to_string(body.length()) + "\r\n";
         response += "Connection: close\r\n\r\n";
         response += "Served by backend: " + std::string(hostname) + "\n";
+        response += body;
         
         send(client_fd, response.c_str(), response.length(), 0);
         close(client_fd);
